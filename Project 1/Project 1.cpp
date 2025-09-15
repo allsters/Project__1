9dc4c71abfa13d95e351115b99103dde553469dc
allst <allst@DESKTOP-I2HBRGL>
2025-09-15 18:34:50 -0400

    Add project files.

---
+++ Project 1/Project 1.cpp
@@ -0,0 +1,15 @@
+// Project 1.cpp 
+//This program displays your initals
+
+#include <iostream>
+using namespace std;
+int main() {
+	char first, middle, last;
+	//store your initials in these variables
+	first = 'A';
+	middle = 'C';
+	last = 'E';
+	cout << first << middle << last << endl;
+	return 0;
+}
+
